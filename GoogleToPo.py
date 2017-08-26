8ebc3f547b7b24f78745b0f955dd6cbc077071fc
mollywww <31369412+mollywww@users.noreply.github.com>
2017-08-26 21:16:16 +0800

    Add files via upload

---
+++ GoogleToPo.py
@@ -0,0 +1,21 @@
+﻿# -*- coding: utf-8 -*-
+# GoogleSheets to po
+msgid = None
+msgtxt = None
+msgstr = None
+with open('msgid.txt', 'r', encoding='utf-8') as f:
+    msgid = [_.strip() for _ in f.readlines()]
+
+with open('msgtxt.txt', 'r', encoding='utf-8') as f:
+    msgtxt = [_.strip() for _ in f.readlines()]
+
+with open('msgstr.txt', 'r', encoding='utf-8') as f:
+    msgstr = [_.strip() for _ in f.readlines()]
+
+if len(msgid) !=  len(msgtxt) != len(msgstr):
+    print('文件長度不同, 請檢查檔案!')
+else:
+    with open('zh_TW.po', 'w', encoding='utf-8') as f:
+        f.write('"Application: Dont\' Starve\\n"\n"POT Version: 2.0\\n"\n\n\n')
+        for i in range(len(msgtxt)):
+            f.write(f'#. {msgtxt[i]}\nmsgctxt "{msgtxt[i]}"\nmsgstr "{msgstr[i]}"\n\n')
